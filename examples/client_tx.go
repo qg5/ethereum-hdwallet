0e55f5dcf30a55a733aceaab8eb8f5809e790bb2
qg5 <87708897+qg5@users.noreply.github.com>
2024-06-17 19:05:30 +0200

    Update client_tx.go

--- examples/client_tx.go
+++ examples/client_tx.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	ethd "github.com/qg5/ethereum-hdwallet/hdwallet"
+	"github.com/qg5/ethereum-hdwallet/ethd"
 	"github.com/qg5/ethereum-hdwallet/transaction"
 )
 
