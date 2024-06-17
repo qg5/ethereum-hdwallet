0c12c18904d0856884c9add7b25d10282484d8d5
qg5 <87708897+qg5@users.noreply.github.com>
2024-06-17 16:42:05 +0200

    Create tx.go

---
+++ examples/tx.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	ethd "github.com/qg5/ethereum-hdwallet/hdwallet"
+	"github.com/qg5/ethereum-hdwallet/transaction"
+)
+
+func main() {
+	wallet, err := ethd.New("misery easy pilot elbow adapt carpet spot sword bless device tuition diet arm elite naive", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path, err := ethd.CreateDerivationPath(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	derived, err := wallet.Derive(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nonce := uint64(0)
+	destination := common.HexToAddress("0x0")
+	amount := big.NewInt(1000000000000000000)
+	gasLimit := uint64(21000)
+	gasPrice := big.NewInt(21000000000)
+
+	tx := transaction.NewTx(nonce, &destination, amount, gasLimit, gasPrice, []byte{})
+	signedTx, err := transaction.SignTx(tx, derived.PrivateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Signed tx hex:", signedTx)
+}
