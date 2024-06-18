69f0f07c0ae700f8fbe78b6024281c681eaaef9b
qg5 <87708897+qg5@users.noreply.github.com>
2024-06-18 07:53:10 +0200

    Create main.go

---
+++ examples/custom_derive/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/qg5/ethereum-hdwallet/ethd"
+)
+
+func main() {
+	wallet, err := ethd.New("misery easy pilot elbow adapt carpet spot sword bless device tuition diet arm elite naive", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path, err := ethd.ParseDerivationPath("m/0/0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	derived, err := wallet.Derive(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Private key: 0x%x\n", crypto.FromECDSA(derived.PrivateKey))
+	fmt.Printf("Public key: 0x%x\n", crypto.CompressPubkey(&derived.PublicKey))
+	fmt.Println("Address:", derived.Address)
+}
